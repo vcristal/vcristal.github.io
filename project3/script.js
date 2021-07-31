93925598f2127f5b5a837cbfc0dbe9e8681cc537
vcristal <gabriela.ariasdelarosa@gmail.com>
2021-07-31 14:08:57 -0400

    Update script.js

--- project3/script.js
+++ project3/script.js
@@ -3,20 +3,20 @@ document.querySelector('.Check').addEventListener('click',function(){
    if(!inputUser){
     document.querySelector('.msg').textContent='No Input!!'
    }//This will give a message if the user did not submit a value
-})// You are listening to user input. You can define the function right away of outside
 
-let randomnumber = Math.floor(Math.random()*100+1);
-let guess =1;
-document.getElementById,('Check').onclick=function() {
-    let answer=document.getElementById('userInput').Value;
-if (randomnumber==answer){
-    console.log('You got the right answer');
-}
-else if (randomnumber>answer){
-    console.log ('Try a smaller number');
-    guess++;
-}
-else {
-    console.log ('Try a greater number')
-}
-}
+   let randomnumber = Math.trunc(Math.random()*100)+1;
+   console.log(randomnumber)
+   let guess =1;
+   if (randomnumber==inputUser){
+       console.log('You got the right answer');
+   }
+   else if (randomnumber>inputUser){
+       console.log ('Try a greater number');
+       guess++;
+   }
+   else {
+       console.log ('Try a smaller number')
+   }
+
+
+})// You are listening to user input. You can define the function right away of outside
