f0a6e709b37d3c137764cf76b57f745d8ae7a024
vcristal <gabriela.ariasdelarosa@gmail.com>
2021-07-30 22:26:33 -0400

    finished

--- assignment7/script.js
+++ assignment7/script.js
@@ -1,4 +1,5 @@
 /* Temperature converter*/
+console.log("This is the temperature converter")
 
 let celsiusT=30
 let farenheit=((celsiusT*9.5)+32)
@@ -6,11 +7,11 @@ console.log(`${celsiusT}°C is ${farenheit}°F`)
 
 farenheit=90
 celsiusT=((farenheit-32)*5/9)
-console.log(`${farenheit}°F is ${celsiusT}°C `)
+console.log(`${farenheit}°F is ${celsiusT.toFixed(2)}°C `)
 
 
 /*BMI comparison*/
-
+console.log("This is the BMI exercise")
 //Data 1 calculations step 1
 let lucasmass=78;
 let johnmass=92;
@@ -21,39 +22,31 @@ let johnheight=1.95;
 let lucasbmi=lucasmass/lucasheight**2;
 let johnbmi=johnmass/johnheight**2;
 
-
-//Data 1 calculations step 3
-let lucasHigherBMI=lucasbmi>johnbmi;
-console.log("It is",lucasHigherBMI,"that Lucas has a higher BMI because Lucas'BMI is",lucasbmi,"while John's BMI is",johnbmi);
-
-//Data 2 calculations step 1
-lucasmass=95;
-johnmass=85;
-lucasheight=1.88;
-johnheight=1.76;
-
-//Data 2 calculations step 2
-lucasbmi=lucasmass/lucasheight**2;
-johnbmi=johnmass/johnheight**2;
-
-//Data 2 calculations step 3
-lucasHigherBMI=lucasbmi>johnbmi;
-console.log(`It is ${lucasHigherBMI} that Lucas has a higher BMI because Lucas'BMI is ${lucasbmi} while John's BMI is ${johnbmi}`);
-
+if(lucasbmi>johnbmi){
+    console.log(`Lucas' BMI (${lucasbmi.toFixed(1)}) is higher than John's (${johnbmi.toFixed(1)})!`)
+}
+else{
+    console.log(`John's BMI (${johnbmi.toFixed(1)}) is higher than Lucas' (${lucasbmi.toFixed(1)})!`) 
+}
 
 /*Baseball teams*/
+console.log("This is the Baseball team exercise using Data Bonus 2")
+
+//Calculate average score of each team using Data 
+netsaver=(97+112+101)/3
+knicksaver=(109+95+106)/3
 
 minscore=100
 team1=100
 team2=100
 
-if (team1>team2&&team1>=minscore){
-    console.log("Team1 wins")
+if (netsaver>knicksaver&&netsaver>=minscore){
+    console.log("The Nets win")
 }
-else if (team2>team1&&team2>=minscore) {
-    console.log("Team2 wins")   
+else if (knicksaver>netsaver&&knicksaver>=minscore) {
+    console.log("The Knicks win")   
 }
-else if (team1=team2&&team1>=minscore){
+else if (netsaver=knicksaver&&netsaver>=minscore){
     console.log("It is a draw")    
 }
 else{
