e5dbdbdd9f91702daf63f0f6e069198c7f2ec38d
vcristal <gabriela.ariasdelarosa@gmail.com>
2021-07-31 15:00:28 -0400

    Update script.js

--- project3/script.js
+++ project3/script.js
@@ -1,23 +1,19 @@
+let randomnumber = Math.trunc(Math.random()*100)+1
+console.log(randomnumber)
 document.querySelector('.Check').addEventListener('click',function(){
-   this.inputUser=Number(document.querySelector('.userInput').value)
-   if(!this.inputUser){
+   inputUser=Number(document.querySelector('.userInput').value)
+   if(!inputUser){
     document.querySelector('.msg').textContent='No Input!!'
    }//This will give a message if the user did not submit a value
+   let guess =1;
+   if (randomnumber==inputUser){
+       console.log('You got the right answer');
+   }
+   else if (randomnumber>inputUser){
+       console.log ('Try a greater number');
+       guess++;
+   }
+   else {
+       console.log ('Try a smaller number')
+   }
 })// You are listening to user input. You can define the function right away of outside
-
-generaterandom:function(){
-  let randomnumber = Math.trunc(Math.random()*100)+1;
-  console.log(randomnumber)
-  let guess =1;
-  if (randomnumber==this.inputUser){
-      console.log('You got the right answer');
-  }
-  else if (randomnumber>this.inputUser){
-      console.log ('Try a greater number');
-      guess++;
-  }
-  else {
-      console.log ('Try a smaller number')
-  }
-
-}
